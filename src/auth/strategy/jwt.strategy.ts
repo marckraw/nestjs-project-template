4f9d30b002bb85435b0d11688efb2381ba667e85
marckraw <marckraw@icloud.com>
2022-02-26 15:37:44 +0100

    feat: add nice decorator to get user data with guard

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -9,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(config: ConfigService, private prisma: PrismaService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: true,
+      ignoreExpiration: false,
       secretOrKey: config.get('JWT_SECRET'),
     });
   }
